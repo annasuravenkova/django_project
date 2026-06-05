5b9f33e208a80f5aff1fdf2aaf88eb8bebba0e04
AnnaS <anna.suravenkova@gmail.com>
2026-06-05 15:55:27 +0300

    feat: added categories views and updated posts to use slug routing

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-
+from blog_app.models import Category
 from blog_app.models import Post
 
 def index(request):
@@ -11,12 +11,12 @@ def posts_list(request):
 
     content = '<h1>Опубликованные статьи</h1><br><br>'
     for post in posts:
-        content += f'<a href="/posts/{post.id}">{post.title}</a> ({post.created_at})<br>'
+        content += f'<a href="/posts/{post.slug}/">{post.title}</a> ({post.created_at})<br>'
 
     return HttpResponse(content)
 
-def posts_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
+def posts_detail(request, post_slug):
+    post = get_object_or_404(Post, slug=post_slug)
     content = f'''
     <h1>{post.title}</h1>
     <p>Автор: {post.author}</p>
@@ -24,4 +24,21 @@ def posts_detail(request, post_id):
     <hr>
     <a href="/posts/">Назад к статьям</a>
     '''
+    return HttpResponse(content)
+
+def categories_list(request):
+    categories = Category.objects.all()
+    content = ''
+    for category in categories:
+        content += f'''
+        <ul><li>{category.title}</li><ul><br><br>
+        '''
+    return HttpResponse(content)
+
+def categories_detail(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    content = f'''<h1>{category.title}</h1>'''
+    posts = Post.objects.filter(category=category, published=True)
+    for post in posts:
+       content += f'''<a href="/posts/{post.slug}/">{post.title}</a><br><br>'''
     return HttpResponse(content)
