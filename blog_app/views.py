651af0c6c540449b33d285e2e925b3d5d7a1816e
AnnaS <anna.suravenkova@gmail.com>
2026-06-09 14:36:02 +0300

    fixed homework-4

--- blog_app/views.py
+++ blog_app/views.py
@@ -28,16 +28,17 @@ def posts_detail(request, post_slug):
 
 def categories_list(request):
     categories = Category.objects.all()
-    content = ''
+    content = '<ul>'
     for category in categories:
         content += f'''
-        <ul><li>{category.title}</li><ul><br><br>
+        <li>{category.title}</li><ul><br><br>
         '''
+    content += '</ul>'
     return HttpResponse(content)
 
 def categories_detail(request, category_id):
     category = get_object_or_404(Category, id=category_id)
-    content = f'''<h1>{category.title}</h1>'''
+    content = f"<h1>{category.title}</h1>"
     posts = Post.objects.filter(category=category, published=True)
     for post in posts:
        content += f'''<a href="/posts/{post.slug}/">{post.title}</a><br><br>'''
