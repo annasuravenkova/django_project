7cee1b7b7d84048c19a75ccecadfcba70230a396
AnnaS <anna.suravenkova@gmail.com>
2026-06-09 15:43:09 +0300

    feat: migrated posts list to templates and configured local static bootstrap

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-
 from blog_app.models import Post
+from .models import Category
 
 def index(request):
     posts = Post.objects.filter(published=True)[:5]
@@ -22,3 +22,21 @@ def posts_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     context = {'post': post}
     return render(request, 'blog/post_detail.html', context)
+
+
+def categories_list(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'blog/categories_list.html', context)
+
+
+def category_detail(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    posts = Post.objects.filter(category=category, published=True)
+    context = {
+        'category': category,
+        'posts': posts
+    }
+    return render(request, 'blog/category_detail.html', context)
