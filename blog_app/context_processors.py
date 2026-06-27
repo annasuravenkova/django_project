4be672295bfd15342c542d285725c13ab9995e28
AnnaS <anna.suravenkova@gmail.com>
2026-06-27 14:15:51 +0300

    lesson-11

---
+++ blog_app/context_processors.py
@@ -0,0 +1,4 @@
+from blog_app.models import Category
+
+def categories_processor(request):
+    return {'nav_categories': Category.objects.all()}
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path("feedback/", include("feedback_app.urls", namespace="feedback")),
     path('users/', include('users_app.urls', namespace='users' )),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
